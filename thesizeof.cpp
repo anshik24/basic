1eb1c56ffd0fb9b0a4a1d4377c30eb9f1f52e2c6
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-03-11 20:07:41 +0530

    the sizeof

---
+++ thesizeof.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <iostream>
+using namespace std;
+
+// Function prototype
+void dataTypes(int, float, double, long long, string);
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+// Function to find size of different data types
+void dataTypes(int a, float b, double c, long long l, string d){
+   float m=b/c;
+   double n= b/a;
+   int o=c/a;
+   long long p=o+l;
+   cout<<sizeof(m)<<" "<<sizeof(n)<<" "<<sizeof(o)<<" "<<sizeof(p)<<endl;
+   
+    cout<<sizeof(d)<<" "<<sizeof(d[3])<<endl;
+
+}
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+    int a;
+    float b;
+    double c;
+    long long l;
+    string d;
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        
+        cin >> a >> b >> c >> l >> d;
+    
+        dataTypes(a, b, c, l, d);
+    }
+    
+    return 0;
+}  // } Driver Code Ends
