bd3fdf2dd4d9ec976bbebd5b9458f4bc5a16f342
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-03-16 09:05:13 +0530

    Red Or Green

---
+++ redORgreen.cpp
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+       int RedOrGreen(int N,string S) {
+           //code here
+           int g=0;
+           int r=0;
+           for(int i=0;i<N;i++){
+               if(S[i]=='G')
+               g++;
+          if(S[i]=='R')
+           r++;
+    } 
+    if(r<g)
+    return r;
+    return g;
+               
+           }
+       
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.RedOrGreen(N,S) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
