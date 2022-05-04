ea97de937f15c08c43d0781308d3121dc8e1f9ee
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-05-04 16:44:06 +0530

    The dice problem

---
+++ the dice problrm.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int oppositeFaceOfDice(int N)
+    {
+        // Write Your Code here
+        int arr[6]={6,5,4,3,2,1};
+        return arr[N-1];
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        int ans  = ob.oppositeFaceOfDice(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
