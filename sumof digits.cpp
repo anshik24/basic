e5110ec1801beb726b62165a69d52a69ab782098
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-05-05 11:25:15 +0530

    sum of digits

---
+++ sumof digits.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+    int sumOfDigits(int N){
+        //code here
+        int digit,sum=0;
+        while(N!=0){
+            digit=N%10;
+            sum+=digit;
+            N=N/10;
+        }
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.sumOfDigits(N) << endl;
+    }
+    return 0; 
+}  // } Driver Code Ends
