a1ed0ffe019059364fce0bcb1623d3f3d89c20ff
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-08-27 22:29:10 +0530

    Create Anshuman's favourite number.cpp

---
+++ Anshuman's favourite number.cpp
@@ -0,0 +1,11 @@
+class Solution{   
+public:
+    string isValid(long long N){
+        // code here 
+        string x="YES";
+        string y="NO";
+        if(N%5==0)
+        return x;
+        return y;
+    }
+};
