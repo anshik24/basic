03d5ab6ab6d15a8046b5d3350c8ff70a1c88a203
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-08-29 21:07:15 +0530

    Create remainder with 7.cpp

---
+++ remainder with 7.cpp
@@ -0,0 +1,14 @@
+class Solution{
+    public:
+    /*You are required to complete this method */
+    int remainderWith7(string n)
+    {
+    int sum=0;
+    for(int i=0;i<n.size();i++){
+        int digit=n[i]-'0';
+        sum=sum*10+digit;
+        sum=sum%7;
+    }
+    return sum;
+    }
+};
